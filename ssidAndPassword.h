e3556427fbbf59e6e9205a7810bd9dfa1823bd54
Joghurt <github@dg4sfw.de>
2026-04-17 21:14:14 +0200

    Structural enhancement

--- ssidAndPassword.h
+++
@@ -1,3 +0,0 @@
-// Edit ssid and password or use Button WPS (automatically activated within 40 seconds after cold start)
-String wifiName     = "No More Mr. WiFi";
-String wifiPassword = "pennygetyourownwifi";
